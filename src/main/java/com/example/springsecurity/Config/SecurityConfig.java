ffa8ef0010b3a4e8d98d707eecde1e980be772e6
denist2322 <parksomang125@gmail.com>
2022-08-12 10:10:32 +0900

    2부 url을 만들다

---
+++ src/main/java/com/example/springsecurity/Config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.springsecurity.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity //스프링 시큐리티를 스프링 필터체인에 등록
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf().disable()
+                        .authorizeRequests(authorize -> authorize.antMatchers("/user/**")
+                        .authenticated()
+                        .antMatchers("/manager/**")
+                        .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                        .anyRequest().permitAll())
+                .build();
+    }
+}
