8eefbf916975f74740f41f51ca45d77e92b158b4
denist2322 <parksomang125@gmail.com>
2022-08-16 10:27:10 +0900

    할거 다했음

--- src/main/java/com/example/springsecurity/Config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/Config/SecurityConfig.java
@@ -4,23 +4,38 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity //스프링 시큐리티를 스프링 필터체인에 등록
+@EnableWebSecurity// 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됩니다.
 public class SecurityConfig {
+
+    // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf().disable()
-                        .authorizeRequests(authorize -> authorize.antMatchers("/user/**")
+                .authorizeRequests(authroize -> authroize.antMatchers("/user/**")
                         .authenticated()
                         .antMatchers("/manager/**")
                         .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                        .antMatchers("/admin/**")
+                        .access("hasRole('ROLE_ADMIN')")
                         .anyRequest().permitAll())
+                //인증 안된 페이지로 가면 로그인 페이지로 보냄
                 .formLogin()
-                .loginPage("/login")
+                .loginPage("/loginForm")
+                .loginProcessingUrl("/login")// login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해준다.
+                // /login 안만들어도 됨
+                .defaultSuccessUrl("/")
                 .and()
                 .build();
     }
+
 }
--- src/main/java/com/example/springsecurity/IndexController.java
+++ src/main/java/com/example/springsecurity/IndexController.java
@@ -1,52 +1,61 @@
 package com.example.springsecurity;
 
+import com.example.springsecurity.Domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @GetMapping({"","/"})
+    @GetMapping({"", "/"})
     public String index() {
         return "index";
     }
 
     @GetMapping("/user")
     @ResponseBody
-    public String user(){
+    public String user() {
         return "user";
     }
 
     @GetMapping("/manager")
     @ResponseBody
-    public String manager(){
+    public String manager() {
         return "manager";
     }
 
     @GetMapping("/admin")
     @ResponseBody
-    public String admin(){
+    public String admin() {
         return "admin";
     }
 
     // 시큐리티 로그인 페이지로 안가고 여기로 오도록 강제함.
-    @GetMapping("/login")
-    @ResponseBody
-    public String login(){
-        return "login";
+    @GetMapping("/loginForm")
+    public String login() {
+        return "loginForm";
     }
 
-    @GetMapping("/join")
-    @ResponseBody
-    public String join(){
-        return "join";
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "joinForm";
     }
 
-    @GetMapping("/joinProc")
-    @ResponseBody
-    public String joinProc(){
-        return "회원가입이 완료됨";
+    @PostMapping("/join")
+    public String join(User user) {
+        System.out.println("user");
+        System.out.println(user);
+        user.setRole("ROLE_USER");
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        return "redirect:/loginForm";
     }
 
 }
